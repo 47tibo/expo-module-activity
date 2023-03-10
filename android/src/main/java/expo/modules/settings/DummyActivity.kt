47a1792bf00a145e3fdafa5d161c71eb905e9734
Thibaut Gilbert <47tibo@gmail.com>
2023-03-10 14:14:33 +0100

    fix(activity): specify result ok with intent

--- android/src/main/java/expo/modules/settings/DummyActivity.kt
+++ android/src/main/java/expo/modules/settings/DummyActivity.kt
@@ -1,14 +1,26 @@
 package expo.modules.settings
 
+import android.content.Intent
 import android.os.Bundle
 import android.os.PersistableBundle
 import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
+import java.util.*
+import kotlin.concurrent.schedule
 
 
 class DummyActivity: AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
         super.onCreate(savedInstanceState, persistentState)
+
+        Timer().schedule(3000) {
+            Log.i("DummyActivity", "timeout ! finish activity")
+            val data = Intent()
+            data.putExtra("hello", "world")
+            setResult(RESULT_OK, data)
+            finish()
+        }
+
         Log.i("DummyActivity", "onCreate")
     }
 
